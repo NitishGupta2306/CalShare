c816344738581c786d4007ade97e7ac3b30fdfd1
Nitish <123796155+Nitish23062002@users.noreply.github.com>
2024-01-19 15:24:15 -0800

    New Swift Project

---
+++ CalShare/CalShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalShareApp.swift
+//  CalShare
+//
+//  Created by Nitish Gupta on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CalShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalShare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalShare
+//
+//  Created by Nitish Gupta on 1/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
