2dc35980c5f7ff69e25045b29cde2f48f84d76b3
Nitish <nitishgupta2306@gmail.com>
2024-01-19 15:29:53 -0800

    Fresh Start

--- CalShare/CalShareApp.swift
+++ CalShare/CalShareApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CalShareApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandingPage()
         }
     }
 }
---
+++ CalShare/LandingPage.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CalShare
+//
+//  Created by Nitish Gupta on 1/19/24.
+//
+
+import SwiftUI
+
+struct LandingPage: View {
+    var body: some View {
+        VStack {
+            Text("Fresh Start")
+        }
+    }
+}
+
+#Preview {
+    LandingPage()
+}
