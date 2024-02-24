38592260f9be2f3ab895d2ece591b51cf3d96740
Jonathan Vazquez <j0n4than9100@gmail.com>
2024-02-24 12:50:26 -0800

    Added Code for app to handle sign in when used

--- CalShare/CalShareApp.swift
+++ CalShare/CalShareApp.swift
@@ -7,12 +7,19 @@
 
 import SwiftUI
 import FirebaseCore
+import GoogleSignIn
+import GoogleSignInSwift
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
+                   open url: URL,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
+      
+    // Google Sign In
+    GIDSignIn.sharedInstance.handle(url)
+    //GIDSignIn.sharedInstance().clientID = "500712914827-4qhbo858nts7n7k8f0iqlsd347480hq5.apps.googleusercontent.com"
 
     return true
   }
@@ -22,6 +29,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 struct CalShareApp: App {
   // register app delegate for Firebase setup
   @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
 
   var body: some Scene {
     WindowGroup {
