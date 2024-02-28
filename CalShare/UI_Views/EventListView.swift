cc3a17d6b0b29258fc5948228c23b071543fb8d0
Andrew Helbig <helbigventures@gmail.com>
2024-02-28 15:19:48 -0800

    created view for calendar events that can be easily customized

---
+++ CalShare/UI_Views/EventListView.swift
@@ -0,0 +1,18 @@
+//
+//  EventListView.swift
+//  CalShare
+//
+//  Created by Drew Helbig on 2/28/24.
+//
+
+import SwiftUI
+
+struct EventListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventListView()
+}
