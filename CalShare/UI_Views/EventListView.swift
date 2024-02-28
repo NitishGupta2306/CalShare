4e6fda94e078a900161b245771805f2c40131b2a
Andrew Helbig <helbigventures@gmail.com>
2024-02-28 15:21:26 -0800

    implemented event listing view

--- CalShare/UI_Views/EventListView.swift
+++ CalShare/UI_Views/EventListView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct EventListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @EnvironmentObject var calendar: CalendarViewModel
+  
+  var body: some View {
+    
+      ScrollView {
+          ForEach(calendar.events) { idEvent in
+            VStack(alignment: .center) {
+                  Text("Event: \(idEvent.event.title)")
+                  Text("Date: \(formatDate(idEvent.event.startDate, format: "MM d, yyyy"))")
+                  Text("Start Time: \(formatDate(idEvent.event.startDate))")
+                  Text("End Time: \(formatDate(idEvent.event.endDate))")
+              }
+              .foregroundStyle(.black)
+              .padding()
+            }
+        }
+    }
+  
+    private func formatDate(_ date: Date, format: String = "h:mm a") -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: date)
     }
 }
 
