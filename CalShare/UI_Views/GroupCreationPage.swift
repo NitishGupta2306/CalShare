24849cebe9eec06a3f875e331eb0258dacfad28a
Shubhada Martha <103222835+smartha2003@users.noreply.github.com>
2024-03-13 11:39:42 -0700

    Updated UI for the GroupCreationPage

--- CalShare/UI_Views/GroupCreationPage.swift
+++ CalShare/UI_Views/GroupCreationPage.swift
@@ -13,59 +13,81 @@ struct GroupCreationPage: View {
     var body: some View {
         NavigationStack {
                 ZStack {
-                    VStack {
-                        Spacer()
-                        Text("Create or Join New Group!")
-                            .font(.custom(fontTwo, size: 30.0))
-                            .foregroundColor(Color("PastelGray"))
-                            .fontWeight(.regular)
-                        
+                    VStack (alignment: .leading) {
                         Spacer()
                         
-                        //Goes to DisplayCodePage
-                        Button(action: {
-                            print("Generate QR")
-                            genQR = true
+//                        Image(systemName: "person.3.fill")
+//                            .foregroundColor(Color("PastelOrange"))
+//                            //.padding(.leading, 15)
+//                            .font(.system(size: 190))
+                        
+                        //VStack (alignment: .leading, spacing: 10) {
+                            Text("Create New Group!")
+                                .font(.custom(fontTwo, size: 20.0))
+                                .foregroundColor(Color("PastelGray"))
+                                .fontWeight(.regular)
+                                .padding(20)
                             
-                        }) {
-                            HStack {
-                                Text("Generate QR")
+                            
+                            ZStack {
+                                //Goes to DisplayCodePage
+                                Button(action: {
+                                    print("Generate QR")
+                                    genQR = true
+                                    
+                                }) {
+                                    HStack {
+                                        Text("Generate QR")
+                                            .font(.custom(fontTwo, size: 20.0))
+                                            .foregroundColor(Color("PastelBeige"))
+                                            .bold()
+                                        
+                                        Image(systemName: "qrcode")
+                                            .foregroundColor(Color("PastelBeige"))
+                                            .padding(.leading, 10)
+                                            .font(.system(size: 75))
+                                    }
+                                    .padding(.horizontal, 60)
+                                    .padding(.vertical, 20)
+                                    .background(Color("PastelOrange"))
                                     .foregroundColor(Color("PastelBeige"))
-                                
-                                Image(systemName: "qrcode")
+                                    .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+                                    .frame(width: 350, height: 80)
+                                }
+                            }.padding(20)
+                            
+                            Text("Join New Group!")
+                                .font(.custom(fontTwo, size: 20.0))
+                                .foregroundColor(Color("PastelGray"))
+                                .fontWeight(.regular)
+                                .padding(20)
+                            
+                            ZStack {
+                                // Access Camera and allows currUser to join group linked to QRCode.
+                                Button(action: {
+                                    print("Scan QR")
+                                    self.scanQR.toggle()
+                                }) {
+                                    HStack {
+                                        Text("Scan QR")
+                                            .font(.custom(fontTwo, size: 20.0))
+                                            .foregroundColor(Color("PastelBeige"))
+                                            .bold()
+                                        
+                                        Image(systemName: "qrcode.viewfinder")
+                                            .foregroundColor(Color("PastelBeige"))
+                                            .padding(.leading, 10)
+                                            .font(.system(size: 75))
+                                    }
+                                    .padding(.horizontal, 70) //100
+                                    .padding(.vertical, 20)
+                                    .background(Color("PastelOrange"))
                                     .foregroundColor(Color("PastelBeige"))
-                                    .padding(.leading, 10)
-                                    .font(.system(size: 30))
-                            }
-                            .padding(.horizontal, 100)
-                            .padding(.vertical, 20)
-                            .background(Color("PastelOrange"))
-                            .foregroundColor(Color("PastelBeige"))
-                            .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
-                            .frame(width: 400, height: 100)
-                        }
-                        
-                        // Access Camera and allows currUser to join group linked to QRCode.
-                        Button(action: {
-                            print("Scan QR")
-                            self.scanQR.toggle()
-                        }) {
-                            HStack {
-                                Text("Scan QR")
-                                    .foregroundColor(Color("PastelBeige"))
-                                
-                                Image(systemName: "qrcode")
-                                    .foregroundColor(Color("PastelBeige"))
-                                    .padding(.leading, 10)
-                                    .font(.system(size: 30))
-                            }
-                            .padding(.horizontal, 100)
-                            .padding(.vertical, 20)
-                            .background(Color("PastelOrange"))
-                            .foregroundColor(Color("PastelBeige"))
-                            .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
-                            .frame(width: 400, height: 100)
-                        }
+                                    .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+                                    .frame(width: 350, height: 80)
+                                }
+                            }.padding(20)
+                        //}
                         
                         if gotQR {
                             Text("Got QR")
